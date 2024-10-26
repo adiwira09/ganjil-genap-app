0b147cd7d72684ac115b0d819c1a941bd433e32d
zaki-52 <dzkioplimapuluhdua@gmail.com>
2024-10-26 11:18:11 +0700

    adding frame excel

--- main.py
+++ main.py
@@ -1,8 +1,40 @@
 import tkinter as tk
+from tkinter import ttk
+import pandas as pd
+
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+import time
+import threading
 
 from border import border_frame
 from Header import create_text_frame
 from Webcam import WebcamViewer
+
+def load_excel_data(frame, file_path):
+    try:
+        data = pd.read_excel(file_path)
+        tree = ttk.Treeview(frame, columns=list(data.columns), show="headings")
+
+        column_widths = {
+            "No": 50,
+            "Frame": 80,
+            "Text Plate": 200,
+            "Keterangan": 150
+        }
+
+        for col in data.columns:
+            tree.heading(col, text=col)
+            tree.column(col, minwidth=50, width=column_widths.get(col, 100), anchor="center")
+
+        for index, row in data.iterrows():
+            tree.insert("", "end", values=list(row))
+
+        tree.pack(fill="both", expand=True)
+
+    except Exception as e:
+        error_label = tk.Label(frame, text=f"Error loading file: {e}", fg="red")
+        error_label.pack()
 
 # Create the main application window using tkinter
 root = tk.Tk()
@@ -15,6 +47,7 @@ frame1 = border_frame(root, relx=0.0145, rely=0.02, relwidth=0.625, relheight=No
 create_text_frame(frame1)
 
 frame2 = border_frame(root, relx=0.65, rely=0.02, relwidth=0.34, relheight=0.955)
+load_excel_data(frame=frame2, file_path="file.xlsx")
 
 # Webcam frame
 webcam_viewer = WebcamViewer(root, zoom_percent=75, margin=20)
