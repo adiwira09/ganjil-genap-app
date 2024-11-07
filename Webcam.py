a618b87a93a2a9464995bbeb12ce0c42e6428c8f
adiwira09 <adiwira009@gmail.com>
2024-11-07 23:03:24 +0700

    optimize code for resize frame

--- Webcam.py
+++ Webcam.py
@@ -4,28 +4,29 @@ from PIL import Image, ImageTk
 from tkinter import Label
 
 class WebcamViewer:
-    def __init__(self, root, zoom_percent=100, margin=50):
+    def __init__(self, root, detector, zoom_percent=100, margin=50):
         self.cap = cv2.VideoCapture(0)
         self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
         self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
-        
-        self.zoom_percent = zoom_percent
+
+        self.scale_percent = zoom_percent / 100
+        width = int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH) * self.scale_percent)
+        height = int(self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT) * self.scale_percent)
+        self.dim = (width, height)
+
         self.margin = margin
         self.root = root
 
+        self.detector = detector
         self.label = Label(self.root)
 
     def show_frame(self):
         ret, frame = self.cap.read()
 
         if ret:
-            scale_percent = self.zoom_percent / 100
-            
-            width = int(frame.shape[1] * scale_percent)
-            height = int(frame.shape[0] * scale_percent)
-            dim = (width, height)
+            resized_frame = cv2.resize(frame, self.dim, interpolation=cv2.INTER_AREA)
 
-            resized_frame = cv2.resize(frame, dim, interpolation=cv2.INTER_AREA)
+            self.detector.detect(resized_frame)
 
             cv2image = cv2.cvtColor(resized_frame, cv2.COLOR_BGR2RGB)
             img = Image.fromarray(cv2image)
@@ -47,24 +48,23 @@ class WebcamViewer:
 class VideoPlayer:
     def __init__(self, root, video_path, detector, zoom_percent=100, margin=50):
         self.cap = cv2.VideoCapture(video_path)
-        self.detector = detector
-        self.zoom_percent = zoom_percent
+
+        self.scale_percent = zoom_percent / 100
+        width = int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH) * self.scale_percent)
+        height = int(self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT) * self.scale_percent)
+        self.dim = (width, height)
+
         self.margin = margin
         self.root = root
-
+        
+        self.detector = detector
         self.label = Label(self.root)
 
     def show_frame(self):
         ret, frame = self.cap.read()
 
         if ret:
-            scale_percent = self.zoom_percent / 100
-
-            width = int(frame.shape[1] * scale_percent)
-            height = int(frame.shape[0] * scale_percent)
-            dim = (width, height)
-
-            resized_frame = cv2.resize(frame, dim, interpolation=cv2.INTER_AREA)
+            resized_frame = cv2.resize(frame, self.dim, interpolation=cv2.INTER_AREA)
 
             self.detector.detect(resized_frame)
 
